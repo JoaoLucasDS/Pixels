191fe28745cc1ad82f122b5b6d03c329cdb0b7f0
JoaoLucasDS <jldantassilva@gmail.com>
2022-01-12 14:58:27 -0300

    Working color and size function

--- canvas.js
+++ canvas.js
@@ -1,22 +1,15 @@
 const sizeEl = document.querySelector('.size')
-const resetBtn = document.querySelector('.btn')
+const applyBtn = document.querySelector('.btn')
+const color = document.querySelector('.color')
 
+const radio = document.querySelector('input[type=radio]:checked');
 
 window.addEventListener('load',() => {
    const canvas = document.querySelector("#canvas");
    const ctx = canvas.getContext("2d");
 
-
-   //ctx.imageSmoothingEnabled = false;
-   //ctx.mozImageSmoothingEnabled = false;
-   //ctx.webkitImageSmoothingEnabled = false;
-   //ctx.msImageSmoothingEnabled = false;
-
-   //ctx.canvas.width  = 720;
-   //ctx.canvas.height = 480;
-
-   ctx.canvas.width  = 160;
-   ctx.canvas.height = 90;
+   ctx.canvas.width  = 16*sizeEl.value;
+   ctx.canvas.height = 9*sizeEl.value;
 
    //var
 
@@ -36,9 +29,9 @@ window.addEventListener('load',() => {
    function pixelOnClick(e){
       var mousePositon = getMousePos(canvas,e);
 
-      //ctx.fillStyle = "rgba("+r+","+g+","+b+","+(a/255)+")";
-      ctx.fillRect(mousePositon.x , mousePositon.y, 1,1)
-
+      ctx.fillStyle = color.value;
+      ctx.fillRect(Math.round(mousePositon.x), Math.round(mousePositon.y), 1, 1)
+      return;
 
    }
 
@@ -47,27 +40,28 @@ window.addEventListener('load',() => {
       var dy = Math.abs(y1 - y0);
       var sx = (x0 < x1) ? 1 : -1;
       var sy = (y0 < y1) ? 1 : -1;
-      var err = dx - dy;
+      var error = dx - dy;
 
       while(true) {
+         ctx.fillStyle = color.value;
          ctx.fillRect(x0, y0, 1,1);
          // setPixel(x0, y0); Do what you need to for this
 
          if ((x0 === x1) && (y0 === y1)) break;
-         var e2 = 2*err;
-         if (e2 > -dy) { err -= dy; x0  += sx; }
-         if (e2 < dx) { err += dx; y0  += sy; }
+         var e2 = 2*error;
+         if (e2 > -dy) { error -= dy; x0  += sx; }
+         if (e2 < dx) { error += dx; y0  += sy; }
       }
       return;
-
    }
 
    var count = 0
    var firstPoint;
    var secondPoint;
 
-   function handleMouseClick(e) {
+   function drawLine(e) {
       if (count == 0) {
+         ctx.fillStyle = 'black';
          firstPoint = getMousePos(canvas, e);
          ctx.fillRect(Math.round(firstPoint.x), Math.round(firstPoint.y), 1, 1);
          count = 1;
@@ -82,16 +76,27 @@ window.addEventListener('load',() => {
          console.log(Math.round(firstPoint.x), Math.round(firstPoint.y), Math.round(secondPoint.x), Math.round(secondPoint.y));
          return;
       }
+   }
+
+   function handleClick(e){
+      console.log(radio.value)
+      if (radio.value == 'Pixel') {
+         pixelOnClick(e);
+      }
+      else if (radio.value == 'Line') {
+         drawLine(e);
+      }
 
    }
 
 
-   //eventListeners
-   canvas.addEventListener("mousedown", handleMouseClick);
-   //canvas.addEventListener("click", line(20,20,30,30));
+   canvas.addEventListener("click", handleClick);
 
+   applyBtn.addEventListener('click', function(){
+      ctx.canvas.width  = 16*sizeEl.value;
+      ctx.canvas.height = 9*sizeEl.value;
 
-
+   })
 
 });
 
